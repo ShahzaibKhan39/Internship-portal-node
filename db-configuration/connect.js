77a3505e3125b5a9011bfb86114d2252f82d3662
Shahzaib Khan <sk0963415@gmail.com>
2026-08-17 09:56:56 -0700

    Update project files and fix typos

--- db-configuration/connect.js
+++ db-configuration/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB() {
-  const dbUri = process.env.MONGO_URI || "mongodb+srv://gh4737400_db_user:<db_password>@internship-portal.usvhfug.mongodb.net/internship-portfolio";
+  const dbUri = process.env.MONGO_URI || "mongodb+srv://gh4737400_db_user:<db_password>@internship-portal.usvhfug.mongodb.net";
 
   mongoose.connect(dbUri)
     .then(() => {
