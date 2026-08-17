44ac6658503e022ae8c13f1561cff2a402010d3e
Shahzaib Khan <sk0963415@gmail.com>
2026-08-17 09:44:23 -0700

    Update project files and fix typos

--- db-configuration/connect.js
+++ db-configuration/connect.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 function connectDB() {
-  const dbUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/internship-portfolio";
+  const dbUri = process.env.MONGO_URI || "mongodb+srv://gh4737400_db_user:<db_password>@internship-portal.usvhfug.mongodb.net/internship-portfolio";
 
   mongoose.connect(dbUri)
     .then(() => {
       console.log("Successfully connected to MongoDB");
     })
     .catch(err => {
-      console.error(" Database connection failed error details:", err.message);
+      console.error("Database connection failed error details:", err.message);
     });
 }
 
